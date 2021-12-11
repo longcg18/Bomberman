41890030a3e2fd029cad453679691fd32bb7b144
Phuc Long Nguyen <20021387@vnu.edu.vn>
2021-12-11 17:02:00 +0700

    Update get item sound

--- src/uet/oop/bomberman/Sound/Sound.java
+++ src/uet/oop/bomberman/Sound/Sound.java
@@ -7,8 +7,10 @@ import java.io.File;
 
 public class Sound {
     public final static Media bombExplosion = new Media(new File("Resources/gamesounds/bombexplosion.mp3").toURI().toString());
+    public final static Media getItem = new Media(new File("Resources/gamesounds/getItemSound.wav").toURI().toString());
 
     public final static MediaPlayer bombExplosionSound = new MediaPlayer(bombExplosion);
+    public final static MediaPlayer getItemSound = new MediaPlayer(getItem);
 
     public static void playMedia(MediaPlayer mp) {
         mp.play();
